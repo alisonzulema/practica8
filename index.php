dab4d0ef68cba1066d250a2541ed49c0fbf2f091
alisonzulema <160642586+alisonzulema@users.noreply.github.com>
2024-05-16 14:08:47 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Esto da el resultado de una division</p>';
+
+    
+    $num = 10;
+    $num2 = 2;
+    $result = $num / $num2;
+     echo '<p>10/2</p>';
+    echo "Resultado de la división: " . $result;
+    ?> 
+
+</html>
---
+++ readme.txt
@@ -0,0 +1,11 @@
+DE: Num,Num2
+DS: resultado 
+
+
+Inicio
+Imprimir "Resultado de la división: " 
+Num = 20
+Num2 = 2
+resultado = Num / Num2
+Imprimir "Resultado de la división: " . resultado;
+fin
